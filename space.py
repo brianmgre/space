b65cdefa85689879ee4e73a3f2abebe116ab2109
Brian G <brian.gregorius@gmail.com>
2019-02-12 13:16:39 -0500

    added self and created a way to display map

--- space.py
+++ space.py
@@ -1,2 +1,18 @@
 class Space:
-    def __init__(self, )
+    def __init__(self, width, height):
+
+        # todo: allocate 20 lists to hold space
+        self.space = []
+
+        for i in range(height):
+            row = ['.'] * width
+            self.space.append(row)
+
+        print(self.space)
+
+        # todo: add random stars
+
+        #todo: methods
+
+
+s = Space(15, 10)
